fe0f4f505d6f8bc4e1a89f6eacb715801b763be0
Brandon-Harris <brandonharris177@gmail.com>
2020-07-09 10:08:26 -0600

    word count and no dupes working

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -3,9 +3,18 @@ import random
 # Read in all the words in one go
 with open("input.txt") as f:
     words = f.read()
+    words = words.split()
+
 
 # TODO: analyze which words can follow other words
 # Your code here
+following = {}
+
+for word in words:
+    if 
+        words[word] = words[word+1]
+
+print(words)
 
 
 # TODO: construct 5 random sentences
